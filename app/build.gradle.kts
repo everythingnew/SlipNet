03b14fab9d6341cf42aae9985e1c466eb2212a83
anonvector <258689915+anonvector@users.noreply.github.com>
2026-03-29 16:58:11 +0200

    Bump versionCode to 61 for v2.4.3 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 val minSdkVersion = 24
 val appVersionName = "2.4.3"
-val appVersionCode = 60
+val appVersionCode = 61
 val cargoProfile = (findProperty("CARGO_PROFILE") as String?) ?: run {
     val isRelease = gradle.startParameter.taskNames.any { it.contains("Release", ignoreCase = true) }
     if (isRelease) "release" else "debug"
